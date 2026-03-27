886e29d0cb0d33f648c31e06f6b97bbdddacecee
AhmedHisham1 <a7med.hish@gmail.com>
2026-03-28 00:35:08 +0100

    Bump version to 0.2.1 in __init__.py and pyproject.toml

--- pyautoflip/__init__.py
+++ pyautoflip/__init__.py
@@ -5,7 +5,7 @@ A Python library for automatic video reframing that intelligently crops videos
 to different aspect ratios while preserving salient content.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 import logging
 from typing import Optional, Union
